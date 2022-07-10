cacd48aba59f95c4c553a0b488f19cf58a978924
Sunkari Pradeep Kumar <935049@cognizant.com>
2022-07-10 21:10:40 +0530

    initial commit

---
+++ com.tweetapp.consoleApp/GetWeatherData.cs
@@ -0,0 +1,33 @@
+﻿using com.tweetapp.domain;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.tweetapp.consoleApp
+{
+    public class GetWeatherData
+    {
+        public  async Task<List<WeatherForecast>> GetData()
+        {
+            using HttpClient client = new HttpClient();
+            UriBuilder builder = new UriBuilder("https://localhost:44387/weatherforecast");
+
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            var httpResponse = await client.GetAsync(builder.Uri);
+
+            if (!httpResponse.IsSuccessStatusCode)
+            {
+                var response = await httpResponse.Content.ReadAsStringAsync();
+                string exception = JObject.Parse(response).SelectToken("message").ToString();
+                throw new HttpRequestException("Error:" + exception);
+            }
+            var content = await httpResponse.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<WeatherForecast>>(content);
+        }
+    }
+}
---
+++ com.tweetapp.consoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using com.tweetapp.api.Controllers;
+using com.tweetapp.domain;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace com.tweetapp.consoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            
+            await Task.Delay(TimeSpan.FromSeconds(30));
+            Console.WriteLine("Hello World!");
+            GetWeatherData weatherData = new GetWeatherData();
+            var data = await weatherData.GetData();
+            foreach (var item in data)
+            {
+                Console.WriteLine(item.Date + " " + item.Summary + " " + item.TemperatureC + " " + item.TemperatureF);
+            }
+        }
+
+       
+    }
+
+    
+}
